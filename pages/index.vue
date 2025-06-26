abcf7c7b99d2aceff08818f5986410119ba7cdf2
sothearo-kay <sothearo.kay@techostartup.center>
2025-06-26 10:55:29 +0700

    feat: tenant form

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,22 @@
+<script setup lang="ts">
+const tenantName = ref('');
+
+const goToTenant = async () => {
+  if (tenantName.value) {
+    const currentHost = window.location.host;
+    const protocol = window.location.protocol;
+    const url = `${protocol}//${tenantName.value}.${currentHost}`;
+
+    await navigateTo(url, { external: true });
+  }
+};
+</script>
+
 <template>
   <div>
     <h1>Multi-Tenant App</h1>
-    <form @submit.prevent="">
-      <input name="tenant" placeholder="your-subdomain" required />
+    <form @submit.prevent="goToTenant">
+      <input v-model="tenantName" name="tenant" placeholder="your-subdomain" required />
       <button type="submit">Go to Tenant</button>
     </form>
   </div>
